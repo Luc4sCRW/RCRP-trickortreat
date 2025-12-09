9b7bb8e0671afb962b92b434bb85164162921caf
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2025-12-09 22:16:02 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -10,7 +10,7 @@ RegisterServerEvent('trickortreat:spawnNPC', function(index)
         return
     end
 
-    -- keine "Bleohnung"
+    -- no reward
     if random <= Config.NoRewardChance + Config.AttackChance then
         TriggerClientEvent('ox_lib:notify', src, {
             title = 'Keiner macht die Tür auf...',
@@ -20,7 +20,7 @@ RegisterServerEvent('trickortreat:spawnNPC', function(index)
         return
     end
 
-    -- "Belohnung"
+    -- reward
     TriggerClientEvent('trickortreat:spawnNPCClient', src, house.npcSpawn)
 
     Wait(2000)
@@ -32,43 +32,3 @@ RegisterServerEvent('trickortreat:spawnNPC', function(index)
         type = 'success'
     })
 end)
-
---[[RegisterServerEvent('trickortreat:spawnNPC', function(index)
-    local src = source
-    local house = Config.Houses[index]
-    if not house then return end
-
-    -- NPC clientseitig spawnen
-    TriggerClientEvent('trickortreat:spawnNPCClient', src, house.npcSpawn)
-
-    -- kurze Wartezeit, während NPC animiert
-    Wait(2000)
-
-    local random = math.random(1, 100)
-
-    if random <= Config.AttackChance then
-        Wait(500)
-        TriggerClientEvent('trickortreat:attackPlayer', src)
-        return
-    end
-
-    if random <= Config.NoRewardChance + Config.AttackChance then
-        Wait(500)
-        TriggerClientEvent('ox_lib:notify', src, {
-            title = 'Der Typ schüttelt den Kopf...',
-            description = 'Heute gibt’s wohl nichts für dich!',
-            type = 'error'
-        })
-        return
-    end
-
-    -- Belohnung geben
-    local item = Config.Rewards[math.random(1, #Config.Rewards)]
-    exports.ox_inventory:AddItem(src, item, 1)
-
-    TriggerClientEvent('ox_lib:notify', src, {
-        title = 'Süßes oder Saures',
-        description = ('Der Typ gibt dir ein(e) %s.'):format(item),
-        type = 'success'
-    })
-end)]]
